5d5e67354c3183216cbc04cf15c0dfe214bebfb2
leakyroot <rootleaky@gmail.com>
2025-03-08 18:30:23 +0100

    c: cell class

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from window import *
 
 def main():
     win = Window(1280, 900)
-    lin = Line(Point(10, 40), Point(690, 500))
-    win.draw_line(lin, "purple")
+    #lin = Line(Point(10, 40), Point(690, 500))
+    cell = Cell(100,50,200,400, win, True,True,True,False)
+    #win.draw_line(lin, "purple")
+    cell.draw()
     win.wait_for_close()
 
 main()
--- window.py
+++ window.py
@@ -40,3 +40,30 @@ class Line:
         canvas.create_line(
             self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
         )
+
+class Cell:
+    def __init__(self, _x1, _x2, _y1, _y2, _win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = _x1
+        self._x2 = _x2
+        self._y1 = _y1
+        self._y2 = _y2
+        self._win = _win
+
+    def draw(self):
+        if self.has_top_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            line.draw(self._win.canvas, "black")
+        if self.has_bottom_wall:
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            line.draw(self._win.canvas, "black")
+        if self.has_left_wall:
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            line.draw(self._win.canvas, "black")
+        if self.has_right_wall:
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            line.draw(self._win.canvas, "black")
+            

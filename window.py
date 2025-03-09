a47a1309a2b9d95e6d3731a967a069f615be3bff
leakyroot <rootleaky@gmail.com>
2025-03-09 15:19:49 +0100

    d: maze class, including breaking up start and exit walls

--- window.py
+++ window.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
@@ -42,7 +43,7 @@ class Line:
         )
 
 class Cell:
-    def __init__(self, _x1, _x2, _y1, _y2, _win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+    def __init__(self, _x1, _x2, _y1, _y2, _win=None, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
         self.has_left_wall = has_left_wall
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
@@ -54,18 +55,32 @@ class Cell:
         self._win = _win
 
     def draw(self):
+        if self._win is None:
+            return
         if self.has_top_wall:
             line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
             line.draw(self._win.canvas, "black")
+        else:
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            line.draw(self._win.canvas, "white")
         if self.has_bottom_wall:
             line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             line.draw(self._win.canvas, "black")
+        else:
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            line.draw(self._win.canvas, "white")
         if self.has_left_wall:
             line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             line.draw(self._win.canvas, "black")
+        else:
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            line.draw(self._win.canvas, "white")
         if self.has_right_wall:
             line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
             line.draw(self._win.canvas, "black")
+        else:
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            line.draw(self._win.canvas, "white")
 
     def draw_move(self, to_cell, undo=False):
         x1 = sum([self._x1, self._x2]) / 2
@@ -77,3 +92,62 @@ class Cell:
             fill = "gray"
         line = Line(Point(x1, y1), Point(x2, y2))
         line.draw(self._win.canvas, fill)
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self.num_rows):
+            row = []
+            for j in range(self.num_cols):
+                cell = Cell(0, 0, 0, 0, self.win, True, True, True, True)
+                row.append(cell)
+            self._cells.append(row)
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self._draw_cell(i, j)
+
+
+
+    def _draw_cell(self, i, j):
+        x1 = self.x1 + j * self.cell_size_x  # j is column
+        y1 = self.y1 + i * self.cell_size_y  # i is row
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        
+        cell = self._cells[i][j]
+        cell.x1 = x1
+        cell.y1 = y1
+        cell.x2 = x2
+        cell.y2 = y2
+        if self.win is not None:
+            cell.draw()
+            self._animate()
+        
+
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.1)
+
+    def _break_entrance_and_exit(self):
+        start = self._cells[0][0]
+        start.has_top_wall = False
+        start.draw()
+
+        print(f"Maze dimensions: {len(self._cells)} rows by {len(self._cells[0])} columns")
+        print(f"Trying to access cell at position: {len(self._cells)-1}, {len(self._cells[0])-1}")
+
+        end = self._cells[-1][-1]
+        print(f"End cell exists: {end is not None}")
+        end.has_bottom_wall = False
+        end.draw()
